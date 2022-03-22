f95eb345f6299c7bc358924474208808bf0537cc
Stephan Knull <stephan.knull@dej-technology.de>
2022-03-22 16:36:22 +0100

    chore: fix duplicate import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
